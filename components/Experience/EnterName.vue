e94f0f21684add4470a3f0196ddd2de186c34b61
Francesco Michelini <hello@francescomichelini.com>
2025-10-09 18:28:05 +0200

    Refactor form

--- components/Experience/EnterName.vue
+++ components/Experience/EnterName.vue
@@ -7,27 +7,30 @@
 			/>
 		</header>
 
-		<form class="form">
+		<form class="form" @submit.prevent="handleSubmit">
 			<div class="input-wrapper">
 				<input
 					class="input | body-3"
 					type="text"
 					name="username"
+					minlength="3"
 					autocomplete="off"
+					required
 					:placeholder="$t('experience_enter_name.input_palceholder')"
 					ref="inputRef"
 					@input="handleInput"
 				/>
 			</div>
-		</form>
 
-		<ButtonGolden
-			class="button"
-			size="square"
-			:data-visible="continueButtonVisible"
-		>
-			<IconArrowRight class="w-4" />
-		</ButtonGolden>
+			<ButtonGolden
+				class="button"
+				size="square"
+				:data-visible="continueButtonVisible"
+				type="submit"
+			>
+				<IconArrowRight class="w-4" />
+			</ButtonGolden>
+		</form>
 	</Container>
 </template>
 
@@ -113,14 +116,22 @@ function setContinueButtonVisible() {
 }
 
 .form {
-	@apply self-center;
+	@apply grid grid-rows-subgrid justify-items-center;
 
-	grid-area: b;
+	grid-area: b / c;
+
+	&:has(.input:not(:valid)) {
+		.button {
+			@apply pointer-events-none opacity-0;
+		}
+	}
 }
 
 .input-wrapper {
-	@apply border-b border-gold/80 border-solid p-3;
+	@apply border-b border-gold/80 border-solid p-3 self-center;
 	@apply lg:py-7 lg:px-12;
+
+	grid-area: b;
 }
 
 .input {
@@ -129,13 +140,9 @@ function setContinueButtonVisible() {
 }
 
 .button {
-	@apply self-center;
+	@apply self-end;
 	@apply duration-500 ease-out;
 
 	grid-area: c;
-
-	&[data-visible='false'] {
-		@apply pointer-events-none opacity-0;
-	}
 }
 </style>
