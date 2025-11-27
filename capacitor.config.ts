e34650a13f9461a78f89dfe6b531d1f4903b3416
Emanuel Baldi <emanuel.baldi@mirror.it>
2025-11-27 16:11:59 +0100

    Update app name to "D&G The One" in capacitor.config.ts and Info.plist. Remove unused printer plugin from Podfile and Podfile.lock. Add new app icon images and update asset catalog structure. Remove obsolete splash screen assets.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
 	appId: "it.mirror.dolcegabbana-theone",
-	appName: "The One",
+	appName: "D&G The One",
 	webDir: ".output/public",
 };
 
