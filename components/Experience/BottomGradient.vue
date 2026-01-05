c76c9c000db35bd0e579d57e82398cda06440ad8
Francesco Michelini <hello@francescomichelini.com>
2026-01-05 14:28:11 +0100

    Add "Scroll down" CTA

--- components/Experience/BottomGradient.vue
+++ components/Experience/BottomGradient.vue
@@ -4,7 +4,7 @@
 
 <style lang="scss" scoped>
 .bottom-gradient {
-	@apply flex items-center justify-center fixed bottom-0 inset-x-0 h-16 z-0 text-center pointer-events-none;
+	@apply flex items-center justify-center fixed bottom-0 inset-x-0 h-16 z-10 text-center pointer-events-none;
 	@apply md:h-36;
 
 	background: linear-gradient(to top, #31190e, ease-in-out, #31190e00);
