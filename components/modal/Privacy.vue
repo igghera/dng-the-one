91f62cb611566995a3c5337c7147df61902f24bf
Francesco Michelini <kekkorider@users.noreply.github.com>
2026-01-20 18:16:16 +0100

    Add IT translation of Privacy Policy

--- components/modal/Privacy.vue
+++ components/modal/Privacy.vue
@@ -11,120 +11,23 @@
 
 			<div class="content" data-lenis-prevent>
 				<div class="content-inner">
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam est
-					dignissimos iusto excepturi maiores reprehenderit animi esse, rerum
-					soluta neque temporibus dolores ut quis ad modi quibusdam vitae
-					ducimus deserunt laborum quasi assumenda itaque et dolorum voluptatum!
-					Veniam obcaecati accusamus facere numquam, sint vel perspiciatis quam
-					beatae amet rem eaque vero explicabo nihil pariatur architecto,
-					cumque, repellendus soluta commodi! Repellendus neque quia numquam
-					error impedit laudantium, quo aliquid quas tempore, quos laborum
-					consequatur voluptatibus eius dicta maxime minima ducimus
-					exercitationem quae eligendi molestias maiores vitae ex ipsam
-					consequuntur. Corporis ex, deserunt, aliquam, distinctio veritatis
-					nisi doloremque illo eius facilis modi mollitia natus a architecto non
-					earum error assumenda delectus voluptate eum temporibus. Tempore
-					laborum provident porro deserunt ad temporibus nulla quas! Ea, itaque
-					incidunt nihil nulla libero totam? Perferendis cumque eveniet
-					excepturi omnis architecto asperiores? Necessitatibus nisi minima
-					ipsam ex commodi. Facilis, mollitia autem aspernatur exercitationem
-					sint quis dolor expedita iste praesentium nisi, assumenda ad veniam
-					officia! Quos iste maiores in consectetur dolorem deleniti voluptatum,
-					numquam illum tempora dolorum totam ullam, dolores unde nobis ratione
-					fugit consequatur quod. Ullam nesciunt quidem ea voluptatem
-					praesentium inventore minus, esse cupiditate laborum quibusdam ipsam
-					veritatis labore saepe possimus obcaecati id amet dignissimos animi
-					asperiores at aperiam rerum sapiente exercitationem omnis! Eius, nisi
-					voluptatem blanditiis sunt velit fugiat qui optio reprehenderit
-					voluptatum consectetur commodi, suscipit repellendus quaerat,
-					dignissimos quasi! Mollitia perferendis nemo quasi nulla culpa illum
-					laudantium sapiente sit, doloribus sequi? Consequatur rem earum ut
-					quos saepe fuga dignissimos suscipit iusto quidem, cum hic. Recusandae
-					sequi consectetur aspernatur modi maiores eaque id maxime quam quae,
-					officia facere iusto nobis culpa nemo suscipit deleniti, perspiciatis
-					cumque cupiditate est? Aliquid quaerat neque quibusdam dolorem.
-					Consequatur, asperiores quaerat? Quo, laborum. Odio, ab quam
-					exercitationem id voluptatem labore accusantium dignissimos ex atque
-					suscipit dolorum ut vel dolores corporis quo voluptatibus. Ratione
-					fugiat facere asperiores enim totam harum incidunt itaque delectus
-					natus. Aspernatur cupiditate, nemo tempora dicta hic eligendi
-					reprehenderit possimus similique accusantium sint sed blanditiis unde
-					voluptates, maiores minima vel cum. Eligendi tenetur amet enim porro.
-					Aliquid, excepturi! Facere, excepturi iusto, vitae veniam architecto
-					mollitia blanditiis, quasi natus cumque voluptas cupiditate molestiae?
-					Ipsam beatae voluptatem a corrupti dolorum in atque incidunt natus
-					recusandae, labore nam quisquam placeat. Nemo amet quidem laudantium
-					quas placeat, magnam tempora unde iste. Quidem maiores incidunt culpa
-					corporis optio minima neque repellat id commodi voluptatibus inventore
-					temporibus esse at laudantium, dolores non facere accusantium autem
-					enim! Eveniet perspiciatis dolor eum alias, accusantium quibusdam.
-					Aliquam quidem doloremque laboriosam nobis hic sunt praesentium facere
-					autem aut voluptates, magnam amet pariatur dignissimos voluptatum
-					sequi repellat molestias optio deleniti atque commodi. Aut odit
-					tempora, dignissimos sunt suscipit natus eligendi rem nam blanditiis
-					sapiente cum ipsum eos voluptatum quasi ratione corporis omnis
-					molestiae laboriosam tenetur vel officiis laborum dolorum porro. Quam,
-					molestiae ab, odit, neque dolorem harum consequatur esse incidunt
-					praesentium cumque animi nihil? Perspiciatis deserunt ullam porro quam
-					eaque. Soluta cum eum, hic magnam ab, quam, aspernatur placeat optio
-					qui culpa fugiat unde doloremque dolor dolorem architecto quod tempora
-					numquam nam vero rerum voluptas amet doloribus sequi! Nesciunt laborum
-					enim ratione ab? Praesentium deserunt debitis mollitia suscipit a,
-					officiis similique doloremque possimus ex consequuntur. Odit error
-					placeat ea ullam quos sapiente unde sunt, perferendis suscipit vel
-					expedita ipsam deserunt pariatur, blanditiis odio possimus fugiat
-					nesciunt ut molestias veniam assumenda in non delectus enim? Vero aut
-					minima ex exercitationem nostrum cupiditate, quam eum culpa id hic
-					vitae animi, a ratione saepe velit repellat! Illo cupiditate
-					distinctio ea beatae vitae, vero earum nobis fuga in perspiciatis
-					minus nihil rerum voluptatem architecto blanditiis possimus itaque
-					nesciunt. Facilis ratione natus qui vel quidem temporibus accusamus
-					maxime impedit! Libero minima voluptatibus incidunt eligendi unde
-					ratione maiores itaque, accusantium tenetur? Corrupti maxime,
-					laudantium magnam blanditiis odio at iusto cum. Non suscipit saepe
-					deleniti, est fugiat voluptates rem harum vel provident numquam
-					distinctio quae repudiandae et nostrum cumque explicabo maxime illo
-					eaque quidem ab doloremque ducimus, sunt omnis? Tempore eius unde
-					distinctio dignissimos illum placeat autem ipsa expedita, excepturi
-					libero voluptates deleniti quo exercitationem obcaecati, iure aperiam
-					explicabo neque. Provident animi, sequi vero at, nam voluptas veniam
-					autem sint impedit incidunt, tempora eveniet. Dolor, dolores.
-					Repellendus pariatur nisi velit rerum. Quae dignissimos, odio
-					cupiditate commodi fuga expedita molestiae nostrum laborum laboriosam
-					maxime magnam dolorem delectus recusandae voluptas omnis atque
-					architecto, ea, est nobis a. Dicta itaque adipisci facilis aliquid,
-					voluptate cupiditate dolorem veniam labore aperiam asperiores ea,
-					expedita officiis laboriosam aut. Officiis dolorum illum eveniet
-					tempora vel molestiae excepturi nobis fugiat? Maxime excepturi saepe,
-					vero temporibus nisi facilis atque, sunt odio vitae, quasi ea illum
-					reiciendis ipsa exercitationem earum esse dolor non eligendi! Tenetur
-					officiis et nobis asperiores, maiores voluptatibus distinctio
-					similique, doloremque sunt laboriosam saepe dolor cum odio recusandae!
-					Iure dolorem debitis nobis voluptatum quasi? Possimus placeat
-					obcaecati sunt alias at corporis adipisci eum maxime, delectus atque
-					quia sit commodi quod? Consequatur qui doloremque accusantium sunt et!
-					In necessitatibus libero, tempore vel dolor minima? Hic repellendus
-					dolorum est doloremque, quis quibusdam officiis consequuntur cumque,
-					delectus nisi reprehenderit, consectetur eius mollitia aliquid labore
-					esse aut enim quo voluptas. Magni accusamus omnis nostrum vitae? Sint,
-					ipsa delectus provident temporibus cum sapiente maiores cumque?
-					Molestias at reprehenderit amet perspiciatis repudiandae quibusdam hic
-					modi blanditiis iure quos, velit vero eveniet possimus ex maxime enim
-					cupiditate impedit voluptatem officia. Maiores sunt aliquid expedita
-					unde in harum ratione, alias quibusdam, earum sint incidunt
-					perspiciatis officia maxime quam numquam facere! Commodi
-					necessitatibus tenetur quis provident ullam molestiae illo
-					voluptatibus, mollitia officia quisquam. Error sed tempore, quia
-					repudiandae, officia obcaecati voluptatum nesciunt quidem doloremque,
-					vel reprehenderit accusantium dolore dignissimos odio. Consequatur
-					quas eum totam facere, tempora a debitis dicta similique pariatur
-					iusto officiis explicabo odio obcaecati molestias voluptas vel?
-					Aliquam commodi quis eum placeat iure. Voluptas vitae a provident
-					ullam, iste earum voluptatibus exercitationem, excepturi nobis illum
-					sapiente vel cumque? Eum eos obcaecati debitis quasi consequatur ex
-					itaque, nulla quaerat dolores dolorem cupiditate non corrupti omnis ab
-					voluptate nihil quos distinctio, ipsa possimus ratione minus.
-					Voluptatum eius non corrupti, laudantium quam fuga?
+					<template v-for="(row, idx) in content" :key="idx">
+						<header v-if="row.type === 'headline'" class="headline">
+							<h3>{{ row.body }}</h3>
+						</header>
+
+						<p
+							v-if="row.type === 'paragraph'"
+							class="paragraph"
+							v-html="row.body"
+						/>
+
+						<ol v-if="row.type === 'ordered_list'" class="ordered-list">
+							<li v-for="(item, idx) in row.items" :key="idx">
+								{{ item }}
+							</li>
+						</ol>
+					</template>
 				</div>
 			</div>
 		</div>
@@ -141,11 +44,21 @@ import { get } from '@vueuse/core'
 const appStore = useAppStore()
 const uiStore = useUiStore()
 
-const { locale } = useI18n()
+const { locale, rt, tm } = useI18n()
 
 const config = useRuntimeConfig()
 
 const innerRef = useTemplateRef('innerRef')
+
+const content = computed(() => {
+	return tm('modal_privacy.content').map(item => {
+		return {
+			type: rt(item.type),
+			body: item.body ? rt(item.body) : null,
+			items: item.items ? item.items.map(i => rt(i)) : null,
+		}
+	})
+})
 
 //
 // Misc
@@ -250,6 +163,31 @@ const close = () => {
 }
 
 .content-inner {
+	@apply flex flex-col gap-y-6;
+
 	padding: 0 toRem(36) toRem(88);
+
+	:deep(.headline),
+	:deep([data-type='headline']) {
+		@apply uppercase;
+
+		font-size: toRem(18);
+	}
+
+	:deep(.subheadline),
+	:deep([data-type='subheadline']) {
+		@apply uppercase;
+
+		font-size: toRem(14);
+	}
+
+	:deep(.ordered-list),
+	:deep([data-type='ordered-list']) {
+		@apply list-decimal list-inside;
+	}
+
+	:deep(:not(.headline) + .headline) {
+		@apply pt-6;
+	}
 }
 </style>
